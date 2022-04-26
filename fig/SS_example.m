4b72a1c93fe93371fa3338101be412fd50473d6b
boc5244 <31375763+boc5244@users.noreply.github.com>
2022-04-26 00:39:56 -0400

    Update SS_example.m

--- fig/SS_example.m
+++ fig/SS_example.m
@@ -1,7 +1,5 @@
 function [] = SS_example()
 %% SS_example:
-
-
 f = 0.5;
 A = 37.5;
 phi = 0;
@@ -29,5 +27,37 @@ ax = subplot(1,1,1) ; cla ; hold on
     plot(t, e, 'b', 'LineWidth', 1)
     
     set(ax, 'Color', 'none', 'XLim', [-0.05*tt 1.05*tt])
+
+%% Learning
+f = 0.5;
+A = [10 50];
+phi = 0;
+fs = 100;
+ts = 1 / fs;
+T = 1/f;
+tt = 10*T;
+
+t = (0:ts:tt)';
+a = linspace(A(1), A(2), length(t))';
+r = A(2)*sin(2*pi*f*t + phi);
+
+gain = 0.9;
+phase = -15;
+
+y = gain*a.*sin(2*pi*f*t + phi + deg2rad(phase));
+e = r - y;
+
+fig = figure (1) ; clf
+set(fig, 'Color', 'w', 'Units', 'inches', 'Position', [2 2 4 2])
+movegui(fig, 'center')
+
+ax = subplot(1,1,1) ; cla ; hold on
+    plot(t, r, 'k', 'LineWidth', 1)
+    plot(t, y, 'r', 'LineWidth', 1)
+    plot(t, a, 'g', 'LineWidth', 1)
+    %plot(t, e, 'b', 'LineWidth', 1)
+    
+    set(ax, 'Color', 'none', 'XLim', [-0.05*tt 1.05*tt])
+    axis tight
 
 end
